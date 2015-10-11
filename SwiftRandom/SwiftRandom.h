4a1fcdb5284cfb144368b625b69e813febc5bc1f
Ben <b.cylin@gmail.com>
2015-10-11 21:54:38 +0800

    Set up targets for both iOS and OS X

--- SwiftRandom/SwiftRandom.h
+++ SwiftRandom/SwiftRandom.h
@@ -5,12 +5,10 @@
 //  Created by Ben on 11/10/2015.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for SwiftRandom.
 FOUNDATION_EXPORT double SwiftRandomVersionNumber;
 
 //! Project version string for SwiftRandom.
 FOUNDATION_EXPORT const unsigned char SwiftRandomVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SwiftRandom/PublicHeader.h>
